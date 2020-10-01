4c60e728e17d62066a047f4e2b0ee36817e0eca7
Tobias Berg-Sørensen <2508tobias@gmail.com>
2020-10-01 13:48:46 +0200

    added stickman 2

---
+++ stickman2/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(1000, 1000);
+}
+function draw() {
+
+background(150);
+
+let Color = 1
+  
+strokeWeight(10)
+stroke(Color)
+circle(200, 200, 100);
+circle(180, 190, 1);
+circle(220, 190, 1);
+rect(200, 250, 1, 200);
+line(185, 220, 215, 220);
+line(200, 300, 50, 200);
+line(200, 455, 50, 600);
+line(200, 300, 350, 250);
+line(200, 455, 350, 600);
+}
+
