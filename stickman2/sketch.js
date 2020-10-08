b4a4088f79ca23577fa03ab1d197c71ddd608b9e
Tobias Berg-Sørensen <2508tobias@gmail.com>
2020-10-08 13:02:48 +0200

    tilføjet bold

--- stickman2/sketch.js
+++ stickman2/sketch.js
@@ -1,8 +1,30 @@
+diameter = 40;
+bevægelse = 20;
+
 function setup() {
-  createCanvas(1000, 1000);
-}
-function draw(){
-  background(220);
-  circle(100,300,50);
+  createCanvas(800, 800);
+  Training=1;
 }
 
+function draw() {
+  background(220);
+  if(bevægelse == 20){
+    gårModHøjre = true;
+  }
+  circle(bevægelse, 400, diameter);
+var gårModVenstre
+
+  if (gårModHøjre == true){
+      bevægelse += 1;
+  }
+  else{
+    bevægelse -= 1;
+  }
+  if (bevægelse == 780){
+      gårModHøjre = false;
+  }
+  if (bevægelse == 20){
+      gårModHøjre = true;
+  }
+
+}
