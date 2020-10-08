38cd31d6a38239f73f0d0c2261b04d93efa82ae8
Tobias Berg-Sørensen <2508tobias@gmail.com>
2020-10-08 14:10:45 +0200

    mouse speed

--- stickman2/sketch.js
+++ stickman2/sketch.js
@@ -8,17 +8,18 @@ function setup() {
 
 function draw() {
   background(220);
-  
+  let hastighed = mouseY;
+
   if(xpos == 20){
     gårModHøjre = true;
   }
   circle(xpos, 400, diameter);
 
   if (gårModHøjre == true){
-      xpos += 1;
+      xpos += 1*(mouseY/100);
   }
   else{
-    xpos -= 1;
+    xpos += -1*(mouseY/100);
   }
   if (xpos == 780){
       gårModHøjre = false;
