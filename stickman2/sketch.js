31fda4e6d130502c2e48649b780bcfe1f6349cbe
Tobias Berg-Sørensen <2508tobias@gmail.com>
2020-10-08 13:25:02 +0200

    ændring i bold

--- stickman2/sketch.js
+++ stickman2/sketch.js
@@ -1,30 +1,29 @@
 diameter = 40;
-bevægelse = 20;
+let xpos = 20;
 
 function setup() {
   createCanvas(800, 800);
-  Training=1;
+  Training = 1;
 }
 
 function draw() {
   background(220);
-  if(bevægelse == 20){
+  if (xpos >= 20) {
     gårModHøjre = true;
   }
-  circle(bevægelse, 400, diameter);
-var gårModVenstre
+  circle(xpos, 400, diameter);
 
-  if (gårModHøjre == true){
-      bevægelse += 1;
+  if (gårModHøjre == true) {
+    xpos += 1;
   }
-  else{
-    bevægelse -= 1;
+  else {
+    xpos -= 1;
   }
-  if (bevægelse == 780){
-      gårModHøjre = false;
+  if (xpos >= 780) {
+    gårModHøjre = false;
   }
-  if (bevægelse == 20){
-      gårModHøjre = true;
+  if (xpos == 20) {
+    gårModHøjre = true;
   }
 
 }
