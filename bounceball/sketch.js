6c73d569c1e6c6811f40b3af83a348b5759417c2
Tobias Berg-Sørensen <2508tobias@gmail.com>
2020-11-26 12:52:07 +0100

    ryddet op i koden

--- bounceball/sketch.js
+++ bounceball/sketch.js
@@ -1,7 +1,7 @@
 let xSpeed = [10, -5, 5, 7, -3];
 let ySpeed = [6, 5, 3, -10, 17];
-let xKoordinater = [100, 200, 300, 400, 500];
-let yKoordinater = [100, 100, 100, 100, 100];
+let xPos = [100, 200, 300, 400, 500];
+let yPos = [100, 200, 150, 200, 573];
 
 
 
@@ -15,27 +15,22 @@ function draw() {
   rect(0, 0, width, height);
   
   let d = 120;
-  let antalBolde = xKoordinater.length;
+  let antalBolde = xPos.length;
 
-  for (let i=0 ; i<antalBolde ; i++){
-    if (xKoordinater[i] > width - d / 2 || xKoordinater[i] < 0 + d / 2) {
+  for (let i=0 ; i < antalBolde ; i++) {
+    if (xPos[i] > width - d / 2 || xPos[i] < 0 + d / 2) {
       xSpeed[i] = -xSpeed[i];
     }
   
-    if (yKoordinater[i] > height - d / 2 || yKoordinater[i] < 0 + d / 2) {
+    if (yPos[i] > height - d / 2 || yPos[i] < 0 + d / 2) {
       ySpeed[i] = -ySpeed[i];
     }
   
-    xKoordinater[i] = xKoordinater[i] + xSpeed[i];
-    yKoordinater[i] = yKoordinater[i] + ySpeed[i];
+    xPos[i] += xSpeed[i];
+    yPos[i] += ySpeed[i];
   }
 
-  // tegn en cirkel med centrum i (x, y) og diameter d
- for (let i=0; i<antalBolde ; i++){
-    circle(xKoordinater[i], yKoordinater[i], d);
+ for (let i = 0; i < antalBolde; i++) {
+    circle(xPos[i], yPos[i], d);
  }
-  
-
-
-  
 }
